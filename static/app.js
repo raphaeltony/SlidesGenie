dcdc95757f4ad6ed65d6ce4ff59b96d01ad9696f
Reuben <reubendinny@gmail.com>
2023-07-14 16:33:34 +0530

    loading basic

--- static/app.js
+++ static/app.js
@@ -1,9 +1,43 @@
 var submitButton = document.getElementById("submitButton")
-submitButton.addEventListener("click", get_loader);
+submitButton.addEventListener("click", submit_request);
 
-function get_loader() {
+function update_pgbar(perc){
+    var pgbar = document.querySelector('#progress-bar');
+    pgbar.style.width = "25%"
+}
+
+function submit_request() {
     console.log("work");
     var pgbar = document.querySelector('#progress')
 
     pgbar.classList.toggle('d-none')
+
+    setTimeout(update_pgbar,20000)
+
+    event.preventDefault()
+    const userInput = document.getElementById("message").value;
+
+    // console.log(eventname,instname,startdate,enddate,prize,level,cashprize)
+
+    var formdata = new FormData();
+    formdata.append("userInput", userInput);
+
+    var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow"
+    };
+
+    fetch("/submit/", requestOptions)
+        .then((response) => response.text())
+        .then((result) => {
+            console.log(result);
+            alert(result);
+            // window.location.reload()
+        })
+        .catch((error) => {
+            console.log("error", error);
+            alert(result);
+        });
+
 }
