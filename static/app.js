5a7d7c35336d83555286392532bff47618990c67
Raphael <prince4raphael@gmail.com>
2023-07-16 21:24:17 +0530

    progress bar ready

--- static/app.js
+++ static/app.js
@@ -3,24 +3,34 @@ submitButton.addEventListener("click", submit_request);
 
 function update_pgbar(perc){
     var pgbar = document.querySelector('#progress-bar');
-    pgbar.style.width = "25%"
+    pgbar.style.width = `${perc}%`
 }
 
 function submit_request() {
     console.log("work");
     var pgbar = document.querySelector('#progress')
+    var currentPage = window.location.href.split('/').slice(-1)
 
     pgbar.classList.toggle('d-none')
 
-    setTimeout(update_pgbar,20000)
+    setTimeout(update_pgbar,20000,25)  //20 secs
+    setTimeout(update_pgbar,90000,50)  // 1.5 min
+    setTimeout(update_pgbar,150000,80) // 2.5 min
+    setTimeout(update_pgbar,210000,100) // 3.5
+
 
     event.preventDefault()
     const userInput = document.getElementById("message").value;
+    let styleSelect = "";
+    if(currentPage=='slidesgenie')
+        styleSelect = document.querySelector('input[name="styleSelect"]:checked').value;
 
     // console.log(eventname,instname,startdate,enddate,prize,level,cashprize)
 
     var formdata = new FormData();
     formdata.append("userInput", userInput);
+    if(currentPage=='slidesgenie')
+        formdata.append("styleSelect", styleSelect);
 
     var requestOptions = {
         method: "POST",
@@ -32,8 +42,8 @@ function submit_request() {
         .then((response) => response.text())
         .then((result) => {
             console.log(result);
-            alert(result);
-            // window.location.reload()
+            // alert(result);
+            location.href = result
         })
         .catch((error) => {
             console.log("error", error);
